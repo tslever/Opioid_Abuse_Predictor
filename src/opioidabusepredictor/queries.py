3c7785b037ed4f5da394ac91389b3f0260d4355f
srimann123 <65921067+srimann123@users.noreply.github.com>
2023-07-17 16:52:42 -0400

    Fixed recursive file call queries.py
    Signed-off-by: srimann123 <65921067+srimann123@users.noreply.github.com>

--- src/opioidabusepredictor/queries.py
+++ src/opioidabusepredictor/queries.py
@@ -341,6 +341,6 @@ ORDER BY person_id, visit_occurrence_id
 query_that_results_in_table_of_visit_occurrences_for_cohort = """
     SELECT COUNT(*)
     FROM `""" + os.environ["WORKSPACE_CDR"] + """.visit_occurrence` visit_occurrence 
-    INNER JOIN (""" + queries.query_that_results_in_distinct_IDs_of_patients_with_at_least_one_prescription_of_opioids_and_without_cancer + """) cohort
+    INNER JOIN (""" + query_that_results_in_distinct_IDs_of_patients_with_at_least_one_prescription_of_opioids_and_without_cancer + """) cohort
     ON visit_occurrence.person_id = cohort.person_id
 """
