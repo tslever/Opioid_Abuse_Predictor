0d27d61fbbb264e52b224adcf46eef9684bae1ff
Tom Lever <Thomas.Lever.Business@Gmail.com>
2023-07-24 23:44:44 +0000

    Allowed Sandbox.py to work with generate_feature_matrix_revisited.py

--- opioidabusepredictor/Sandbox.py
+++ opioidabusepredictor/Sandbox.py
@@ -1,10 +1,9 @@
-from opioidabusepredictor import generate_slice_of_feature_matrix as g
+from opioidabusepredictor import generate_feature_matrix_revisited as g
 import os
+import pandas as pd
 
-query_that_results_in_table_criteria = """
-    SELECT *
-    FROM `""" + os.environ["WORKSPACE_CDR"] + """.cb_criteria` cr
-    LIMIT
-"""
-
-data_frame = g.get_data_frame(query_that_results_in_table_criteria)
+data_frame = g.get_data_frame(g.query_that_results_in_feature_matrix)
+print(pd.unique(data_frame["person_id"]))
+print(data_frame)
+deduplicated_data_frame = data_frame.drop_duplicates(subset = ["visit_occurrence_id"])
+print(data_frame.shape[0] - len(deduplicated_data_frame))
