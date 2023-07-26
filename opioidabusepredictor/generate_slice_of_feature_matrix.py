3236ff382d0f843b665bcfea3bd96b6d1776523a
Tom Lever <Thomas.Lever.Business@Gmail.com>
2023-07-26 00:52:51 +0000

    Added query_that_results_in_feature_matrix.py

--- opioidabusepredictor/generate_slice_of_feature_matrix.py
+++ opioidabusepredictor/generate_slice_of_feature_matrix.py
@@ -357,8 +357,44 @@ GROUP BY visit_occurrence_id
 ORDER BY visit_occurrence_id
 """
 
+query_that_results_in_feature_matrix = """
+SELECT
+    person_id,
+    table_of_visit_occurrences_for_undersample.visit_occurrence_id,
+    visit_start_date,
+    has_Anxiety,
+    has_Bipolar_disorder,
+    has_Depressive_disorder,
+    has_Hypertensive_disorder,
+    has_Opioid_abuse,
+    has_Opioid_dependence,
+    has_Pain,
+    has_Rhinitis,
+    has_Non_Opioid_Substance_abuse,
+    is_exposed_to_ibuprofen,
+    is_exposed_to_buprenorphine,
+    is_exposed_to_nelaxone,
+    is_exposed_to_fentanyl,
+    is_exposed_to_morphine,
+    is_exposed_to_oxycodone,
+    is_exposed_to_hydromorphone,
+    is_exposed_to_aspirin,
+    is_exposed_to_codeine,
+    is_exposed_to_tramadol,
+    is_exposed_to_nalbuphine,
+    is_exposed_to_meperidine,
+    is_exposed_to_naltrexone,
+    is_exposed_to_acetaminophen
+FROM (""" + query_that_results_in_table_of_distint_person_IDs_visit_occurrence_IDs_and_visit_start_dates_for_undersample + """) table_of_visit_occurrences_for_undersample
+LEFT JOIN (""" + query_that_results_in_conditions_feature_matrix + """) conditions_feature_matrix
+ON table_of_visit_occurrences_for_undersample.visit_occurrence_id = conditions_feature_matrix.visit_occurrence_id
+LEFT JOIN (""" + query_that_results_in_drugs_feature_matrix + """) medications_feature_matrix
+ON table_of_visit_occurrences_for_undersample.visit_occurrence_id = medications_feature_matrix.visit_occurrence_id
+ORDER BY person_id, visit_occurrence_id
+"""
+
 if __name__ == "__main__":
     data_frame = get_data_frame(
-query_that_results_in_drugs_feature_matrix
+query_that_results_in_feature_matrix
     )
     print(data_frame)
