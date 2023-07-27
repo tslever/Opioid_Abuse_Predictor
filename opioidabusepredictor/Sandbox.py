da1ff4b7d6ed38127597f4681a41dc2e21fdcda4
Tom Lever <Thomas.Lever.Business@Gmail.com>
2023-07-27 00:06:33 +0000

    In Sandbox.py, generated data_frame_of_reference_events

--- opioidabusepredictor/Sandbox.py
+++ opioidabusepredictor/Sandbox.py
@@ -1,13 +1,7 @@
 import pandas as pd
 
 data_frame = pd.read_csv("data/Slice_Of_Feature_Matrix.csv")
+data_frame = data_frame.sort_values(by = ["person_id", "visit_start_datetime"])
+data_frame_of_events_where_patient_is_exposed_to_Opioids = data_frame[data_frame["is_exposed_to_Opioids"] == 1]
+data_frame_of_reference_events = data_frame_of_events_where_patient_is_exposed_to_Opioids.drop_duplicates(subset = ["person_id", "is_exposed_to_Opioids"])
 
-# TODO: Tom wants to use (person_id, visit_occurrence_id) as unique primary key in feature matrix.
-# Write code that ensures that ascending order by visit_occurrence_id implies
-# ascending order by visit_start_date and vice versa.
-
-data_frame_sorted_by_visit_start_date = data_frame.sort_values(by = "visit_start_date")
-#data_frame_sorted_by_visit_occurrence_id = data_frame.sort_values(by = "visit_occurrence_id")
-#print(data_frame_sorted_by_visit_start_date["visit_occurrence_id"].to_list() == data_frame_sorted_by_visit_occurrence_id["visit_occurrence_id"].to_list())
-
-print(data_frame_sorted_by_visit_start_date[["person_id", "visit_occurrence_id", "visit_start_date"]])
