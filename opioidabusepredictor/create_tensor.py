e784c4a1e301027b8eae9eed4e509042bf2515d3
Tom Lever <Thomas.Lever.Business@Gmail.com>
2023-07-28 04:01:09 +0000

    Fixed visit_start_datetime and a reference to a data frame, and resized tensor according to torch.nn.Module

--- opioidabusepredictor/create_tensor.py
+++ opioidabusepredictor/create_tensor.py
@@ -4,18 +4,18 @@ import torch
 
 def get_input_tensor_and_ground_truth(person_id, df, reference_event): # Where reference visit can either visit occurrence id or start datetime
     data_frame_corresponding_to_person_id = df[df["person_id"] == person_id]
-    data_frame_corresponding_to_prior_to_reference_event = df[df["visit_startdate"] <= reference_event]
+    data_frame_corresponding_to_prior_to_reference_event = df[df["visit_start_datetime"] <= reference_event]
     data_frame_corresponding_to_after_reference_event = df[df[visit_startdate] > reference_event]
 
-    will_abuse_opioids_indicator = df["has_Opioid_abuse"].max()
+    will_abuse_opioids_indicator = data_frame_corresponding_to_after_reference_event["has_Opioid_abuse"].max()
 
     # Generate input tensor
     data_frame_corresponding_to_prior_to_reference_event.drop(columns = ["person_id", "visit_occurrence_id", "visit_startdate"], inplace = True)
     numpy_array_corresponding_to_prior_to_reference_event = df.values
     num_visits_for_person = len(numpy_array_corresponding_to_prior_to_reference_event)
     num_features_for_visit = len(numpy_array_corresponding_to_prior_to_reference_event[0])
-    
+
     input_tensor = torch.tensor(df.values) # Input tensor is now a numpy array of 2D with values from df but without columns
-    input_tensor = input_tensor.resize_(1, num_visits_for_person, num_features_for_visit)
+    input_tensor = input_tensor.resize_(num_visits_for_person, 1, num_features_for_visit) # TODO: consider swapping num_visits_for_person and 1
 
     return will_abuse_opioids_indicator, input_tensor
